a9d232fe55d17257befdb9975f1739fd12994b76
cma129 <claudiamhahn@gmail.com>
2020-02-20 11:36:10 -0500

    mobile landscape mq added

--- script.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-    $('.hamburger').click(function() {
-        $('.menu').addClass('show');
-    });
-    $('.exit-responsive').click(function() {
-        $('.menu').removeClass('show');
-    });
-});
